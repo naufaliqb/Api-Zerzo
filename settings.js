6ed7f2a704aed00139234cf9dc720ab8e36fca50
naufaliqb <130411480+naufaliqb@users.noreply.github.com>
2023-05-14 14:37:10 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ require('dotenv').config()
 
 //_______________________ ┏  Main Setting  ┓ _______________________\\
 
-creator = '乂𝘼𝙡𝙞𝙥乂' // Nama 
+creator = 'Iqbal' // Nama 
 port = 8080 //port host
-LimitApikey = 200 // Limit Apikey default
+LimitApikey = 2000 // Limit Apikey default
 
 //___________ ┏  Database Mongodb Setting  ┓ ___________\\
 
